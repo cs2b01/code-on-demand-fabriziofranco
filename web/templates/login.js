b4e1463d3ec8283491419daed0b81778cfc13afe
fabriziofranco <fabrizio.franco@utec.edu.peUt3c2675clearREADME.md it config --global user.email fabrizio.franco@utec.edu.pe>
2019-06-08 21:55:27 -0500

    Tarea terminada

--- web/templates/login.js
+++ web/templates/login.js
@@ -1,5 +1,7 @@
-function getData(){
-        $('#action').html("Authenticating...");
+    function getData(){
+        document.getElementById("ok").style.display = "none";
+        document.getElementById("un").style.display = "none";
+        document.getElementById("waiting").style.display = "inline";
         var username = $('#username').val();
         var password = $('#password').val();
         var message = JSON.stringify({
@@ -14,12 +16,15 @@ function getData(){
             data : message,
             dataType:'json',
             success: function(response){
-                //alert(JSON.stringify(response));
-                $('#action').html(response['statusText']);
-            },
+
+                },
             error: function(response){
-                //alert(JSON.stringify(response));
-                $('#action').html(response['statusText']);
-            }
+                if(response['status']==401){
+                document.getElementById("waiting").style.display = "none";
+                document.getElementById("un").style.display = "inline";}
+                 else{
+                 document.getElementById("waiting").style.display = "none";
+                document.getElementById("ok").style.display = "inline";
+                  }}
         });
     }
